5d13d865253511c99300426e44e6c580537cbf91
daya <dayajuangmahaputra@gmail.com>
2022-11-08 09:52:11 +0900

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,27 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] inputs = {-1600,1600,2017,2000, 1924};
+        boolean ans;
+
+        for (int input : inputs)
+        {
+            ans = isLeapYear(input);
+
+            System.out.println(ans);
+        }
+    }
+
+    public static boolean isLeapYear(int year){
+        // Check if year is valid and if it's divisible by 4
+        if(year < 1 || year > 9999 || (year % 4 != 0))
+            return false;
+
+        // Do the leap year calculation
+        if(year % 100 != 0)
+            return true;
+
+        return (year % 400 == 0);
     }
 }
