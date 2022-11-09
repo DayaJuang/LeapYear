77a899e3fa639e67e2274bb5d1b7d62fa6ba3fed
daya <dayajuangmahaputra@gmail.com>
2022-11-09 13:26:26 +0900

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,6 +11,9 @@ public class Main {
 
             System.out.println(ans);
         }
+
+        int temp = getDaysInMonth(2,2018);
+        System.out.println(temp);
     }
 
     public static boolean isLeapYear(int year){
@@ -23,5 +26,27 @@ public class Main {
             return true;
 
         return (year % 400 == 0);
+    }
+
+    public static int getDaysInMonth(int month, int year){
+        // Check if input is valid
+        if((month < 1 || year < 1) || (month > 12 || year > 9999))
+            return -1;
+
+        // Check if the year is Leap Year or not
+        boolean isLeap = isLeapYear(year);
+
+        // Print every month day
+        switch (month){
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                return 31;
+            case 2:
+                if(isLeap)
+                    return 29;
+                else
+                    return 28;
+            default:
+                return 30;
+        }
     }
 }
